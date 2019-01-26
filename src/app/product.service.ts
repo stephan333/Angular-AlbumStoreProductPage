f016d75281d3e7afc74a7d16c684236dba3a14f8
Stephan Ramke <stephan.ramke@web.de>
2019-01-26 20:12:41 +0100

    Completed the module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,6 +13,6 @@ export class ProductService {
   getAlbum(id: number): Observable<Album> {
     return this._http
       .get(this._albumUrl)
-      .map((response: Response) => <Album>response.json());
+      .map((response: Response) => <Album>(response.json()));
   }
 }
