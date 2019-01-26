af07165c510ac3e43d0051610b0a4329fd6583c9
Stephan Ramke <stephan.ramke@web.de>
2019-01-26 21:46:54 +0100

    Completed the module

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -15,6 +15,6 @@ export class ProductListComponent implements OnInit {
   ngOnInit() {
     this._productService
       .getProducts()
-      .subscribe(response => (this.products = response));
+      .subscribe(response => this.products = response);
   }
 }
